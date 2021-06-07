3e502d08c74b097f7203c773a59ddf97c8f93b39
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-07 20:52:39 +0000

    ✍🏽 Adds a demo for ejs

--- express_server.js
+++ express_server.js
@@ -14,8 +14,15 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get("/hello",(req,res) => {
-  res.send("<html><body>Hello <b>\nWorld</b></body></html>\n")
-})
+  res.send("<html><body>Hello <b>World</b></body></html>\n")
+});
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+app.get("/fetch", (req, res) => {
+  res.send(`a = {a}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
