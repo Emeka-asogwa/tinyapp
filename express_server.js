a097ff9895b4720cb4c91a98112f222ac362044b
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-07 21:18:11 +0000

    ✍🏽 adds the ejs render

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
