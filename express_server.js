c94d297f10f77e14c958e0c11d5944d63fb4ed54
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-07 22:01:49 +0000

    ✍🏽 adds html to the ejs file

--- express_server.js
+++ express_server.js
@@ -8,6 +8,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req,res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
