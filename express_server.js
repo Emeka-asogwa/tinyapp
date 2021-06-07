e8ad182b43b52dc77cc643a52c678b55e3950082
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-07 22:42:54 +0000

    ✍🏽 added the shortURL

--- express_server.js
+++ express_server.js
@@ -12,6 +12,10 @@ app.get("/urls", (req,res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/:shortURL", (req,res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase};
+  res.render("urls_show", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
