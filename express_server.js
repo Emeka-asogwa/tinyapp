cc7a4b41cbb58021eb51bf9cf1382aa7a081ccce
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-08 01:40:44 +0000

    ✍🏽 url shortening

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
@@ -7,6 +8,16 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.post("/urls", (req,res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/urls/new",(req,res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req,res) => {
   const templateVars = { urls: urlDatabase };
